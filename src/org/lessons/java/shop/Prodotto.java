2b4b3d993e96285efa9046ea9878aeebecdc3d7e
danipetro21 <idany346@gmail.com>
2023-06-05 14:22:25 +0200

    add getter and setter methods

--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,4 +1,66 @@
 package org.lessons.java.shop;
 
 public class Prodotto {
+
+    private int codice;
+    private String nome;
+    private String descrizione;
+    private String prezzo;
+    private String iva;
+
+
+    //costruttore
+
+
+    public Prodotto(int codice, String nome, String descrizione, String prezzo, String iva) {
+        this.codice = codice;
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    //getter
+
+    public int getCodice() {
+        return codice;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public String getPrezzo() {
+        return prezzo;
+    }
+
+    public String getIva() {
+        return iva;
+    }
+
+    //setter
+
+    public void setCodice(int codice) {
+        this.codice = codice;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public void setPrezzo(String prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    public void setIva(String iva) {
+        this.iva = iva;
+    }
 }
